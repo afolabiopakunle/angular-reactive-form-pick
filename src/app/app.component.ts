1e3eb05a7193eaa3e4b1b4ffd08cceb770d41dfe
afolabiopakunle <opakunleafolabi@gmail.com>
2022-01-20 18:38:06 +0100

    basic form submits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      fullName: new FormControl(),
+      email: new FormControl()
+    })
+  }
+
+  onSubmit() {
+   console.log(this.form.value)
+  }
 }
